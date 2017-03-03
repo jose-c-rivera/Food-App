0d8a17aed2cf816981460c990a82bf191b1b37c6
initdev <rkenne49@uwo.ca>
2017-03-03 04:46:49 -0500

    pseudo working profile commit

--- foodApp/grails-app/domain/foodapp/UserAccount.groovy
+++ foodApp/grails-app/domain/foodapp/UserAccount.groovy
@@ -1,18 +1,18 @@
 package foodapp
 
 class UserAccount {
-    static mapWith = "mongo"
     String userName
     String password
 
     static hasOne = [userProfile : Profile]
 
-    def getProfile(){
-        return userProfile
-    }
-
     static constraints = {
         userProfile nullable: true
         userProfile unique: true
     }
+
+   def getProfile(){
+        return userProfile
+    }
+
 }
